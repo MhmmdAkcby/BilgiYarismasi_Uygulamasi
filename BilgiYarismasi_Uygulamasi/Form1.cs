c9fa30f0a122763e2bced293ef884fadf19ea635
Lenovo <Lenovo@LENOVO>
2023-04-12 14:44:49 +0300

    Proje dosyası ekle.

---
+++ BilgiYarismasi_Uygulamasi/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BilgiYarismasi_Uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int soruno = 0, dogru = 0, yanlis = 0;
+
+        private void BtnB_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text = BtnB.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void BtnC_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text = BtnC.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void BtnD_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text = BtnD.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void BtnA_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = false;
+            BtnB.Enabled = false;
+            BtnC.Enabled = false;
+            BtnD.Enabled = false;
+            BtnSonraki.Enabled = true;
+
+            label5.Text=BtnA.Text;
+            if (label4.Text == label5.Text)
+            {
+                dogru++;
+                LblDogru.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                LblYanlis.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void BtnSonraki_Click(object sender, EventArgs e)
+        {
+            BtnA.Enabled = true;
+            BtnB.Enabled = true;
+            BtnC.Enabled = true;
+            BtnD.Enabled = true;
+            BtnSonraki.Enabled = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible=false;
+
+            soruno++;
+            LblSoru.Text= soruno.ToString();
+            if (soruno == 1) 
+            {
+                richTextBox1.Text = "Cumhuriyet kaç yılında ilan edilmiştşir ?";
+                BtnA.Text = "A.)  1920";
+                BtnB.Text = "B.)  1921";
+                BtnC.Text = "C.)  1922";
+                BtnD.Text = "D.)  1923";
+                label4.Text=BtnD.Text;
+            }
+            if(soruno == 2)
+            {
+                richTextBox1.Text = "Hangi şehir ege bölgesinde yer almaz ?";
+                BtnA.Text = "A.)  İzmir";
+                BtnB.Text = "B.)  Aydın";
+                BtnC.Text = "C.)  Balıkesir";
+                BtnD.Text = "D.)  Manisa";
+                label4.Text = BtnC.Text;
+            }
+            if (soruno == 3)
+            {
+                richTextBox1.Text = "Son kuşlar kitabı hangi yazara aittir ?";
+                BtnA.Text = "A.)  Sait Faik";
+                BtnB.Text = "B.)  Cemel Süreyya ";
+                BtnC.Text = "C.)  Atilla İlhan";
+                BtnD.Text = "D.)  Reşet Nuri";
+                label4.Text = BtnA.Text;
+                BtnSonraki.Text = "Sonuçlar";
+            }
+            if (soruno == 4)
+            {
+                BtnA.Enabled = false;
+                BtnB.Enabled = false;
+                BtnC.Enabled = false;
+                BtnD.Enabled = false;
+                BtnSonraki.Enabled = false;
+                MessageBox.Show("Doğru : " + dogru + "\n" + "Yanlış : " + yanlis);
+            }
+        }
+    }
+}
